ff35a199c24c46ee2acd1bd700acf068728c281b
Alvaro Moure <alvaro.moure1998@gmail.com>
2023-02-17 20:22:16 +0100

    Log file creation

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
+
+typedef enum {
+    NEW_ERROR = 1,
+    ALL_ERRORS,
+    CLEAN,
+    FINISH
+} MENU_OPTIONS;
+
+char* LOGS_FOLDER = "logs/";
 
 /**
  * @brief Request employee identity from stdin. Employee ID must be a four digit long.
@@ -18,6 +28,20 @@ int request_identity();
  */
 void today_date(char* date, int length);
 
+/**
+ * @brief Calculates today's date and create a log file with that date
+ * in the folder LOGS_FOLDER
+ * 
+ * @return int : 1 if everything went well, 0 if an error occurs when creating the file
+ */
+int create_today_log_file();
+
+/**
+ * @brief Display operator's menu and returns the selected option
+ * 
+ * @return int : selected option [1,4]
+ */
+int display_menu();
 
 // MAIN
 int main() {
@@ -25,11 +49,28 @@ int main() {
     int id = request_identity();
     printf("Employee ID: %d\n", id);
 
-    // Today's date
-    char date[15];
-    today_date(date, sizeof(date));
-    printf("Today's date: %s\n", date);
+    int file_created_flag = create_today_log_file();
+    if (!file_created_flag) {
+        printf("Error creating log file, exiting...\n");
+        exit(1);
+    }
 
+    MENU_OPTIONS option = (MENU_OPTIONS) display_menu();
+    switch (option) {
+    case NEW_ERROR:
+        printf("new error\n");
+        break;
+    case ALL_ERRORS:
+        printf("all errors\n");
+        break;
+    case CLEAN:
+        printf("clean\n");
+        break;
+    case FINISH:
+        printf("finish\n");
+    default:
+        break;
+    }
     return 0;
 }
 
@@ -53,9 +94,38 @@ int request_identity() {
     return id;
 }
 
-void today_date(char* date, int length) {
+int create_today_log_file() {
+    // Compute date
     time_t timestamp = time(NULL);
     struct tm* date_tm = localtime(&timestamp);
-    strftime(date, length, "%Y-%m-%d", date_tm);
+    char date[15];
+    strftime(date, sizeof(date), "%Y-%m-%d", date_tm);
+
+    // Create new log file
+    int success = 1;
+    char filename[40];
+    strcpy(filename, LOGS_FOLDER);
+    strcat(filename, date);
+    FILE* file = fopen(filename, "a");
+
+    if (file == NULL)
+        success = 0;
+    
+    fclose(file);
+    return success;
+}
+
+int display_menu() {
+    int option;
+    char* menu_interface = 
+                    "\n\tMENU\n"
+                    "1) Add new error.\n"
+                    "2) Check error list.\n"
+                    "3) Clean errors.\n"
+                    "4) Finish workday.\n\n"
+                    "Choose option: ";
+    printf("%s", menu_interface);
+    scanf("%d", &option);
+    return option;
 }
 
